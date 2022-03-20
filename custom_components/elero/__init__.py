049604c134d45f232371148b22af926ebb95a5b3
Daniel Figus <daniel.figus@sap.com>
2022-03-20 17:46:50 +0100

    Fix Shutdown Error
    The handler for the Homeassistant shutdown event is missing the event parameter and
    results in a TypeError whe Homeassistant is stopped. This commit fixes this.

--- custom_components/elero/__init__.py
+++ custom_components/elero/__init__.py
@@ -151,7 +151,7 @@ def setup(hass, config):
     ELERO_TRANSMITTERS = EleroTransmitters(transmitters_config)
     ELERO_TRANSMITTERS.discover()
 
-    def close_serial_ports():
+    def close_serial_ports(event):
         """Close the serial port."""
         ELERO_TRANSMITTERS.close_transmitters()
 
